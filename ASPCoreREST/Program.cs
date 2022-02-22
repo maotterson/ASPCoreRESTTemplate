da0a459c5bb2a54f33027627b448409ff6e5bd56
maotterson <otterson.2@gmail.com>
2022-02-22 01:10:35 -0500

    add service+implementation overload for singleton

--- ASPCoreREST/Program.cs
+++ ASPCoreREST/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<PersonRepository>();
+builder.Services.AddSingleton<IPersonRepository, PersonRepository>();
 
 var app = builder.Build();
 
